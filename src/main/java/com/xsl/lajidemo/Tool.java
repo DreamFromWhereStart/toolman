da52527dd5ac0d6514658b2df1189060ce6a8955
DreamFromWhereStart <1581650562@qq.com>
2020-11-16 16:26:57 +0800

    test

---
+++ src/main/java/com/xsl/lajidemo/Tool.java
@@ -0,0 +1,15 @@
+package com.xsl.lajidemo;
+
+/**
+ * @author ：xsl
+ * @date ：Created in 2020/11/16 16:19
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+
+public class Tool {
+    public static void main(String[] args) {
+        System.err.println("123");
+    }
+}
---
+++ src/test/java/com/xsl/lajidemo/LajidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsl.lajidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LajidemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
